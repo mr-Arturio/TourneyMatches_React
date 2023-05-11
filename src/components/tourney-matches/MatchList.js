3186687a786448f1b85406250c626b01354af35a
ArturT <mr.arturio@gmail.com>
2023-05-11 16:31:42 -0400

    create MatchList & Match files

---
+++ src/components/tourney-matches/MatchList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Match from "./Match";
+
+function MatchList() {
+  return (
+    <section className='PlayerList MatchList'>
+      <h1>Match List</h1>
+      <Match />
+    </section>
+  )
+}
+
+export default MatchList
