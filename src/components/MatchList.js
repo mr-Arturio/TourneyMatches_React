648c56ea715e27a2268eb100612a481a82018817
ArturT <mr.arturio@gmail.com>
2023-05-12 23:47:38 -0400

    refactor and finish

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -3,14 +3,26 @@ import Match from "./Match";
 
 function MatchList(props) {
 
-  const oneMatch = props.matchData[0];
+  const matches = props.matchData.map(match => {
+    return (
+      <Match
+        key={match.matchNumber}
+        players={match.players}
+        winner={match.winner}
+        scoreDifference={match.scoreDifference}
+      />)
+    
+  });
 
   return (
     <section className='PlayerList MatchList'>
       <h1>Match List</h1>
-      <Match players={oneMatch.players} winner={oneMatch.winner} scoreDifference={oneMatch.scoreDifference} />
-    </section>
-  )
-}
 
-export default MatchList
+      {matches}
+
+    </section>
+  );
+};
+
+
+export default MatchList;
