f89fe04cd4249af2f1f3d7288b75fba16993cc43
ArturT <mr.arturio@gmail.com>
2023-05-12 22:08:06 -0400

    refactor, import to App.js

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,15 +1,12 @@
 import React from "react";
-import playerData from '../data/playerData';
-import matchData from '../data/matchData';
 import Player from "./Player";
-import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
+
 
 function PlayerList(props) {
-  const playerDataArray = preparePlayerData(playerData);
-  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
-  const onePlayer = parsedPlayerData[0];
-
-  return (
+ 
+  const onePlayer = props.playerData[0];
+  
+    return (
     <section className='PlayerList'>
       <h1>Current participating player</h1>
       <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins}/>
